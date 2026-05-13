697d58094fcba29c280b56cdd26809592b9af445
STANLEY JEREMY GUTIERREZ TUME <jeremygurierreztume@gmail.com>
2026-05-13 15:39:56 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SafeTrack.AuthService.Controllers
+{
+    [ApiController]
+    [Route("api/v1/auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return Ok(new
+            {
+                message = "User registered successfully"
+            });
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok(new
+            {
+                message = "Login successful",
+                token = "jwt-token-generated"
+            });
+        }
+
+        [HttpGet("validate-token")]
+        public IActionResult ValidateToken()
+        {
+            return Ok(new
+            {
+                message = "Token valid"
+            });
+        }
+    }
+}
